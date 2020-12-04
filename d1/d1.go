56de150655b686281eb19c4b46c4e62761c68fb6
Oskar Hahn <mail@oshahn.de>
2020-12-04 17:14:01 +0100

    Day 1 optimisation

--- d1/d1.go
+++ d1/d1.go
@@ -2,11 +2,12 @@ package d1
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 	"strings"
 )
 
-func parseInput(input string) ([]int, error) {
+func parseSlice(input string) ([]int, error) {
 	var nums []int
 	for _, line := range strings.Split(input, "\n") {
 		if line == "" {
@@ -24,42 +25,59 @@ func parseInput(input string) ([]int, error) {
 
 // D1a solves day1 a
 func D1a(input string) (string, error) {
-	nums, err := parseInput(input)
-	if err != nil {
-		return "", fmt.Errorf("can not load input: %w", err)
-	}
+	lines := strings.Split(input, "\n")
+	m := make(map[int]bool, len(lines))
+	nums := make([]int, 0, len(lines))
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
 
-	for i, n1 := range nums {
-		for j, n2 := range nums {
-			if i == j {
-				continue
-			}
-			if n1+n2 != 2020 {
-				continue
-			}
-			return strconv.Itoa(n1 * n2), nil
+		i, err := strconv.Atoi(line)
+		if err != nil {
+			return "", fmt.Errorf("invalid input `%s`: %w", line, err)
+		}
+
+		if i < 1010 {
+			nums = append(nums, i)
+		} else {
+			m[i] = true
 		}
 	}
+
+	for _, num := range nums {
+		if m[2020-num] {
+			return strconv.Itoa(num * (2020 - num)), nil
+		}
+	}
+
 	return "", fmt.Errorf("no correct values :(")
 }
 
 // D1b solves day1 b
 func D1b(input string) (string, error) {
-	nums, err := parseInput(input)
+	nums, err := parseSlice(input)
 	if err != nil {
 		return "", fmt.Errorf("can not load input: %w", err)
 	}
 
-	for i, n1 := range nums {
-		for j, n2 := range nums {
-			for k, n3 := range nums {
-				if i == j || i == k || j == k {
+	sort.Ints(nums)
+
+	for i := len(nums) - 1; i >= 0; i-- {
+		for j := i - 1; j >= 0; j-- {
+			if nums[i]+nums[j] > 2020 {
+				continue
+			}
+
+			//
+			for k := 0; k < j; k++ {
+				if nums[i]+nums[j]+nums[k] > 2020 {
+					break
+				}
+				if nums[i]+nums[j]+nums[k] != 2020 {
 					continue
 				}
-				if n1+n2+n3 != 2020 {
-					continue
-				}
-				return strconv.Itoa(n1 * n2 * n3), nil
+				return strconv.Itoa(nums[i] * nums[j] * nums[k]), nil
 			}
 
 		}
